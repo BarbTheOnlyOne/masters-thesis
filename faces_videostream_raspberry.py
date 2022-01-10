e86e88e1f012d2331024307bade7e5431734531d
BarbTheOnlyOne <79636293+BarbTheOnlyOne@users.noreply.github.com>
2022-01-10 18:14:32 +0100

    Update faces_videostream_raspberry.py
    First ajustment test

--- faces_videostream_raspberry.py
+++ faces_videostream_raspberry.py
@@ -22,7 +22,7 @@ data = pickle.loads(open(args["encodings"], "rb").read())
 detector = cv2.CascadeClassifier(args["cascade"])
 
 # Intialize the video stream, sleep to let the camera sensor to warm up
-print("[INFO] Strating the video stream...")
+print("[INFO] Starting the video stream...")
 # vs = VideoStream(src=0).start() # This line is for USB cam
 vs = VideoStream(usePiCamera=True).start() # This line is for Raspi cam
 time.sleep(2.0)
@@ -30,68 +30,80 @@ time.sleep(2.0)
 # Start the FPS counter
 fps = FPS().start()
 
+# Counter for which image to detect
+image_counter = 0
+
 # Loop the frames from the video stream
 while True:
+
     # Grab the frame from the threaded video stream, resize it to 500px (faster processing)
     frame = vs.read()
     frame = imutils.resize(frame, width=500)
+    image_counter += 1
 
-    # Convert the input frame from BGR to RGB(for face recognition) and to grayscale (for face detection)
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # If the counter reached set amount, detect face
+    if (image_counter == 5):
+        # Don't forget to reset the counter, dummy
+        image_counter = 0
 
-    # Detect faces in the greyscale frame
-    rectangles = detector.detectMultiScale(grayscale, 
-                                           scaleFactor=1.1, 
-                                           minNeighbors=5, 
-                                           minSize=(30, 30))
+        # Convert the input frame from BGR to RGB(for face recognition) and to grayscale (for face detection)
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # OpenCV returns boxes in format of (x, y, w, h), but (top, right, bottom, left) is needed
-    boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rectangles]
+        # Detect faces in the greyscale frame
+        rectangles = detector.detectMultiScale(grayscale, 
+                                            scaleFactor=1.1, 
+                                            minNeighbors=5, 
+                                            minSize=(30, 30))
 
-    # Next make the embeddings for each face bounding box
-    encodings = face_recognition.face_encodings(rgb, boxes)
-    names = []
+        # OpenCV returns boxes in format of (x, y, w, h), but (top, right, bottom, left) is needed
+        boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rectangles]
 
-    # Loop through the encodings
-    for encoding in encodings:
-        # Attempt to match faces in the input with known ones
-        matches = face_recognition.compare_faces(data["encodings"], encoding)
-        name = "Unknown"
+        # Next make the embeddings for each face bounding box
+        encodings = face_recognition.face_encodings(rgb, boxes)
+        names = []
 
-        # Check to see if match was found
-        if True in matches:
-            # Find indexes of all matched faces, initialize dictionary to count the number of votes for each face
-            matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-            counts = {}
+        # Loop through the encodings
+        for encoding in encodings:
+            # Attempt to match faces in the input with known ones
+            matches = face_recognition.compare_faces(data["encodings"], encoding)
+            name = "Unknown"
 
-            # Loop over the matched indexes and mantain a count for each recognized face 
-            for i in matchedIdxs:
-                name = data["names"][i]
-                counts[name] = counts.get(name, 0) + 1
+            # Check to see if match was found
+            if True in matches:
+                # Find indexes of all matched faces, initialize dictionary to count the number of votes for each face
+                matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+                counts = {}
 
-            # Determine the recogniýzed face with the largest number of votes
-            name = max(counts, key=counts.get)
+                # Loop over the matched indexes and mantain a count for each recognized face 
+                for i in matchedIdxs:
+                    name = data["names"][i]
+                    counts[name] = counts.get(name, 0) + 1
 
-        names.append(name)
+                # Determine the recognized face with the largest number of votes
+                name = max(counts, key=counts.get)
 
-    # Loop through the recognized faces
-    for ((top, right, bottom, left), name) in zip(boxes, names):
-        # Draw the predicted face name on the image
-        cv2.rectangle(frame, 
-                      (left, top), 
-                      (right, bottom), 
-                      (0, 255, 0), 
-                      2)
-        y = top - 15 if top - 15 > 15 else top + 15
-        cv2.putText(frame, 
-                    name, 
-                    (left, y), 
-                    cv2.FONT_HERSHEY_SIMPLEX, 
-                    0.75, 
-                    (0, 255, 0), 
-                    2)
-    
+            names.append(name)
+            
+
+
+        # Loop through the recognized faces
+        for ((top, right, bottom, left), name) in zip(boxes, names):
+            # Draw the predicted face name on the image
+            cv2.rectangle(frame, 
+                        (left, top), 
+                        (right, bottom), 
+                        (0, 255, 0), 
+                        2)
+            y = top - 15 if top - 15 > 15 else top + 15
+            cv2.putText(frame, 
+                        name, 
+                        (left, y), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 
+                        0.75, 
+                        (0, 255, 0), 
+                        2)
+
     # Display the image to screen
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
