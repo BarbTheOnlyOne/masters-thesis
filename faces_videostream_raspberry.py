289dd33fb34feca223d88e4ebc49f35667ad671e
BarbTheOnlyOne <79636293+BarbTheOnlyOne@users.noreply.github.com>
2022-02-22 12:42:07 +0100

    Script rework
    Restructured the script a bit for easier orientation, tried to uplift some parts that are heavy on computing

--- faces_videostream_raspberry.py
+++ faces_videostream_raspberry.py
@@ -20,40 +20,39 @@ ap.add_argument("-s", "--display", type=int, required=True, help="If the capture
 ap.add_argument("-o", "--output", type=str, required=True, help="Name of the output file only!")
 args = vars(ap.parse_args())
 
-# Load known faces with OpenCV's Haar cascade
-print("[INFO] Loading encodings and Haar face detector...")
+# Intilize and load detectors, known faces etc.
+print("[INFO] Loading encodings and detectors...")
 data = pickle.loads(open(args["encodings"], "rb").read())
-detector = cv2.CascadeClassifier(args["cascade"])
+detector = cv2.CascadeClassifier(args["cascade"]) # Face detector
+hog = cv2.HOGDescriptor() # Persons detector
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+fourcc = cv2.VideoWriter_fourcc(*"MJPG") # video writer
 
 # Intialize the video stream, sleep to let the camera sensor to warm up
 print("[INFO] Starting the video stream...")
 # vs = VideoStream(src=0).start() # This line is for USB cam
 vs = VideoStream(usePiCamera=True).start() # This line is for Raspi cam
 time.sleep(2.0)
-
 # Start the FPS counter
 fps = FPS().start()
-
-#Initialize the HOG detector for persons
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-# Set up for the video writer
-fourcc = cv2.VideoWriter_fourcc(*"MJPG")
 
 # Counter for which image to detect, snippet numbers and also flag for writer
 image_counter = 0
 video_time_start = 0
-snipper_number = 0
-# Email realted things
+snippet_number = 0
+# Email realted variables
 receiver = "YOUR_MAIL@gmail.com"
 body = "Alert! The secured spaces has been compromised!"
+
+# Initilize array for found names, as well as default name
+names = []
+name = "Unknown"
 
 # Loop the frames from the video stream
 while True:
     # Grab the frame from the threaded video stream, resize it to 500px (faster processing)
     frame = vs.read()
     frame = imutils.resize(frame, width=500)
-    image_counter += 1
 
     # Convert the input frame from BGR to RGB(for face recognition) and to grayscale (for face detection)
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -64,35 +63,30 @@ while True:
     # If person was detected, start recording / or reset timer
     if present_persons:
         video_time_start = time.time()
-        full_snippet_name = args["output"] + f"_{snipper_number}" + ".avi"
+        full_snippet_name = args["output"] + f"_{snippet_number}" + ".avi"
         writer = cv2.VideoWriter(full_snippet_name, 
                             fourcc, 20, 
                             (680, 480), 
                             True)  
 
-    # If the counter reached set amount, detect face
-    if (image_counter == 3):
-        # Don't forget to reset the counter, dummy
-        image_counter = 0
+    # Detect faces in the greyscale frame
+    rectangles = detector.detectMultiScale(grayscale, 
+                                        scaleFactor=1.1, 
+                                        minNeighbors=5, 
+                                        minSize=(30, 30))
 
-        # Detect faces in the greyscale frame
-        rectangles = detector.detectMultiScale(grayscale, 
-                                            scaleFactor=1.1, 
-                                            minNeighbors=5, 
-                                            minSize=(30, 30))
+    # OpenCV returns boxes in format of (x, y, w, h), but (top, right, bottom, left) is needed
+    boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rectangles]
 
-        # OpenCV returns boxes in format of (x, y, w, h), but (top, right, bottom, left) is needed
-        boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rectangles]
-
+    if image_counter == 0:
         # Next make the embeddings for each face bounding box
         encodings = face_recognition.face_encodings(rgb, boxes)
-        names = []
 
         # Loop through the encodings
         for encoding in encodings:
+            names = []
             # Attempt to match faces in the input with known ones
             matches = face_recognition.compare_faces(data["encodings"], encoding)
-            name = "Unknown"
 
             # Check to see if match was found
             if True in matches:
@@ -110,22 +104,27 @@ while True:
 
             names.append(name)
 
-        # Loop through the recognized faces
-        for ((top, right, bottom, left), name) in zip(boxes, names):
-            # Draw the predicted face name on the image
-            cv2.rectangle(frame, 
-                        (left, top), 
-                        (right, bottom), 
-                        (0, 255, 0), 
-                        2)
-            y = top - 15 if top - 15 > 15 else top + 15
-            cv2.putText(frame, 
-                        name, 
-                        (left, y), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 
-                        0.75, 
-                        (0, 255, 0), 
-                        2)
+    # Counter for face encodings check
+    image_counter += 1
+    if image_counter == 10:
+        image_counter = 0
+
+    # Loop through the recognized faces
+    for ((top, right, bottom, left), name) in zip(boxes, names):
+        # Draw the predicted face name on the image
+        cv2.rectangle(frame, 
+                    (left, top), 
+                    (right, bottom), 
+                    (0, 255, 0), 
+                    2)
+        y = top - 15 if top - 15 > 15 else top + 15
+        cv2.putText(frame, 
+                    name, 
+                    (left, y), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 
+                    0.75, 
+                    (0, 255, 0), 
+                    2)
     
     # This makes sure, that the recording runs for 10 seconds at least
     if video_time_start != 0 and (time.time() - video_time_start) < 10:
@@ -133,6 +132,7 @@ while True:
     else:
         send_email.send_mail(receiver, body, full_snippet_name)
         writer.release()
+        snippet_number += 1
 
     # Show the image based on the arguments
     if args["display"] > 0:
