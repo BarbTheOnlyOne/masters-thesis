66c579850c16a98c6ec3c82acb1efe41bab8c6e5
BarbTheOnlyOne <79636293+BarbTheOnlyOne@users.noreply.github.com>
2022-04-06 12:15:38 +0200

    email update

--- send_email.py
+++ send_email.py
@@ -11,7 +11,7 @@ def send_mail(receiver, body, filename):
     ---
     """
 
-    yag = yagmail.SMTP("michal.barborik96", "bdzlxafxdijcdbhz")
+    yag = yagmail.SMTP("email", "random_pass")
     yag.send(
         to=receiver,
         subject="INTRUDER ALERT",
